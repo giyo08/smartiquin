6d3a740a364b20e937fad49957f5bd8c5d829c31
Gerardo Benitez <gbenitez@msa.com.ar>
2019-04-25 21:13:00 -0300

    elimino funcionalidad para sensor de temperatura, incorporo funciones para switch

--- Sistema-Embebido/botiquin.ino
+++ Sistema-Embebido/botiquin.ino
@@ -1,12 +1,11 @@
 int MINUTO = 60000;
 int TIEMPO_CHEQUEO = 2*MINUTO;
-byte TEMPERATURA_MIN = 5; 
-byte TEMPERATURA_MAX = 20; 
 byte HUMEDAD_MIN = 20; 
 byte HUMEDAD_MAX = 90;
 byte LUMINOSIDAD_MIN = 10; 
 byte LUMINOSIDAD_MAX = 70; 
-byte ventilador = 9; 
+byte servo01 = 8;  
+int abierto = 1; 
 
 /**
  * Esta funcion obtiene los valores minimos y maximos de cada
@@ -27,26 +26,6 @@ void esperar_intervalo(){
   delay(TIEMPO_CHEQUEO);
 }
 
-byte leer_temperatura(){
-  return 30; 
-}
-
-/**
- * Chequea que la temperatura no exeda los rangos minimos y maximo
- * Caso contrario, actua en consecuencia llamando a los actuadores.
- */
-void chequear_temperatura(){
-  byte temperatura = leer_temperatura(); 
-  if(temperatura > TEMPERATURA_MAX){
-    encender_ventilador();  
-  }
-
-  if(temperatura < TEMPERATURA_MIN){
-    encender_actuador_temperatura_minima();
-  }
-    
-};
-
 /**
  * Chequea que la humedad no exeda los rangos minimos y maximos 
  * caso contrario, actua en consecuencia llamando a los actuadores
@@ -62,26 +41,45 @@ void chequear_humedad(){};
 void chequear_luminosidad(){};
 
 
-void encender_ventilador(){
-    digitalWrite(ventilador, HIGH); 
-}
 
-void apagar_ventilador(){
-  digitalWrite(ventilador, LOW);  
+
+
+void actuar_switch_abierto(swith){
+  estado = digitalRead(switch)
+  if(estado == abierto){
+    //contar que se saco una pastilla del switch correspondiente
+    
+  }
 }
 
 /**
- * Definir que hara esta funcion
- */
-void encender_actuador_temperatura_minima(){
-  
+Este metodo se usa para identificar que alguno de los switch
+han sido desactivados, es decir, se tomo la caja de medicamentos
+de uno de los slots
+*/
+void chequear_extraccion(){
+  actuar_switch_abierto(swith01);
+  actuar_switch_abierto(swith02);
+  actuar_switch_abierto(swith03);
 }
 
+/**
+ * Realiza la apertura de una puerta, es decir llama a actuar al servomotor
+ * para que mueva un pestillo
+ */
+void chequear_apertura(){
+  if(abrir_pestillo == 1){
+    digitalWrite(servo01, HIGH); 
+    abrir_pestillo = 0;   
+  }
+}
+
+
 void loop() {
-  // put your main code here, to run repeatedly:
-  chequear_temperatura(); 
+  // put your main code here, to run repeatedly: 
   chequear_humedad(); 
-  chequear_luminosidad(); 
-  
+  chequear_luminosidad();
+  chequear_extraccion(); 
+  chequear_apertura();
   esperar_intervalo();
 }
