0be64621356327a371e4c5066a9b33280468c191
Gonza <gonzalocoradini@gmail.com>
2019-05-26 13:03:30 -0300

    Agregado Funcionalidad Abrir/Cerrar con shake
    Uso de sensor acelerometro

--- Android/Smartiquin/app/src/main/java/com/example/smartiquin/MainActivity.java
+++ Android/Smartiquin/app/src/main/java/com/example/smartiquin/MainActivity.java
@@ -1,5 +1,10 @@
 package com.example.smartiquin;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -28,6 +33,13 @@ public class MainActivity extends AppCompatActivity {
     private static final String S_CERRADO = "CERRADO";
 
     ///Sensores
+    private SensorManager sm;
+    private Sensor sensorShake;
+
+    ///Variables para Shake
+    private static final float LIMITE_SHAKE = 75;
+    private long ultShake = 0;
+    private Vibrator v;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,6 +68,12 @@ public class MainActivity extends AppCompatActivity {
             tvEstBot.setText(S_CERRADO);
             btnCerrar.setEnabled(false);
         }
+
+        v = (Vibrator) getSystemService(VIBRATOR_SERVICE);
+
+        ///Asigno sensores
+        sm = (SensorManager) getSystemService(SENSOR_SERVICE);
+        sensorShake = sm.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
 
     }
 
@@ -99,7 +117,75 @@ public class MainActivity extends AppCompatActivity {
         btnAbrir.setEnabled(true);
     }
 
+    public void habilitarShake(View v){
 
+        ///SI HABILITO ABRIR/CERRAR AGITANDO
+        if(switchShake.isChecked()){
+            btnAbrir.setEnabled(false);
+            btnAbrir.setEnabled(false);
 
+            ///HABILITO LA LECTURA DEL SENSOR
+            sm.registerListener(shakeSensorListener,sensorShake,SensorManager.SENSOR_DELAY_GAME);
+        }
+        ///NO ESTA SELECCIONADO, habilito los botones que corresponden
+        else{
+            if(estadoBotiquin == E_ABIERTO){
+                btnAbrir.setEnabled(false);
+                btnCerrar.setEnabled(true);
+            }
+            else{
+                btnCerrar.setEnabled(false);
+                btnAbrir.setEnabled(true);
+            }
+
+            ///DESHABILITO LA LECTURA DEL SENSOR
+            sm.unregisterListener(shakeSensorListener);
+        }
+    }
+
+    SensorEventListener shakeSensorListener = new SensorEventListener() {
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+
+            long tiempoAct = System.currentTimeMillis();
+
+            if((tiempoAct - ultShake) > 300){
+
+                float x = event.values[0];
+                float y = event.values[1];
+                float z = event.values[2];
+
+                double a = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2) + Math.pow(z, 2)) - SensorManager.GRAVITY_EARTH;
+
+                /*float gX = x/SensorManager.GRAVITY_EARTH;
+                float gY = x/SensorManager.GRAVITY_EARTH;
+                float gZ = x/SensorManager.GRAVITY_EARTH;
+
+                float a = (float)Math.sqrt(gX * gX + gY * gY + gZ * gZ);*/
+
+                if(a > LIMITE_SHAKE){
+
+                    ultShake = tiempoAct;
+
+                    if(estadoBotiquin == E_CERRADO){
+                        tvEstBot.setText(S_ABIERTO);
+                        estadoBotiquin = E_ABIERTO;
+                        v.vibrate(100);
+                    }
+                    else{
+                        tvEstBot.setText(S_CERRADO);
+                        estadoBotiquin = E_CERRADO;
+                        v.vibrate(100);
+                    }
+                }
+            }
+
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+        }
+    };
 
 }
