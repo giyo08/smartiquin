4bdf0d4fb4ff78e79c82611186330698215273eb
Gerardo Benitez <gbenitez@msa.com.ar>
2019-05-27 08:42:26 -0300

    cambios para agregar pruebas con buzzer

--- Sistema-Embebido/botiquin.ino
+++ Sistema-Embebido/botiquin.ino
@@ -1,55 +1,70 @@
 // pines digitales
-byte switch01 = 3; 
+byte switch01 = 3;
 byte switch02 = 4;
 byte switch03 = 5;
+byte humedad01 = 13;
 byte LED_Red01 = 7;
 byte LED_Blue01 = 11;
 byte servo01 = 11; //en principio se trata de un led azul
-
-byte pulsador01 = 12; //Por ahora deshabilitado 
-byte ldr01 = A4; 
+byte buzzer01 = 11; 
+byte pulsador01 = 12; //Por ahora deshabilitado
+byte ldr01 = A4;
 
 //parametros
 int MINUTO = 60000;
-int TIEMPO_CHEQUEO = 2*MINUTO;
-byte HUMEDAD_MIN = 20; 
+int TIEMPO_CHEQUEO = 2 * MINUTO;
+byte HUMEDAD_MIN = 20;
 byte HUMEDAD_MAX = 90;
-byte LUMINOSIDAD_MIN = 10; 
-byte LUMINOSIDAD_MAX = 70; 
-  
+byte LUMINOSIDAD_MIN = 10;
+byte LUMINOSIDAD_MAX = 70;
+
 int abierto = 0;
 int cerrado = 1;
 int pulsado = 1;
 int encendido = 1;
-int apagado = 0; 
-int abrir_pestillo = 0; 
+int apagado = 0;
+int abrir_pestillo = 0;
 
 //relacionadas al tiempo y las esperas
-unsigned long tiempo = 0; 
-unsigned long tiempo_anterior = 0; 
-unsigned long intervalo = 200; 
+unsigned long tiempo = 0;
+unsigned long tiempo_anterior = 0;
+unsigned long intervalo = 200;
 
-bool intervalo_cumplido(){
-  return ((tiempo - tiempo_anterior) > intervalo);  
-}
-
-bool intervalo_cumplido_p(unsigned long inicial, unsigned long previo, unsigned long espera){
-  return ((inicial - previo) > espera);
+/**
+   Determina el tiempo que se tomara entre un cheque de estados
+   y otro.
+*/
+bool intervalo_cumplido() {
+  return ((tiempo - tiempo_anterior) > intervalo);
 }
 
 /**
- * Esta funcion obtiene los valores minimos y maximos de cada
- * parametro a comprobar, se comunica con un microservicio
- * que le sirve los valores actualizados
- */
-void obtener_parametros_externos(){
+   Determina si se cumplio un intervalo particular de tiempo,
+   compara un tiempo inicial y previo contra un tiemp de espera
+   Parametros:
+   - actual: es el tiempo actual
+   - previo: es el tiempo tomado antes de iniciar el proceso a controlar
+   - espera: es la cantidad de tiempo que debe transcurrir para indicar si se
+   cumplio el intervalo
+*/
+bool intervalo_particular_cumplido(unsigned long actual, unsigned long previo, unsigned long espera) {
+  return ((actual - previo) > espera);
+}
+
+/**
+   Esta funcion obtiene los valores minimos y maximos de cada
+   parametro a comprobar, se comunica con un microservicio
+   que le sirve los valores actualizados
+*/
+void obtener_parametros_externos() {
   //conectarse y obtener valores
 }
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(servo01, OUTPUT); 
+  pinMode(servo01, OUTPUT);
   pinMode(pulsador01, INPUT);
+  pinMode(buzzer01, OUTPUT);
   //digitalWrite(pulsador01, HIGH);
   Serial.begin(9600);
 
@@ -59,113 +74,149 @@ void setup() {
 
 
 /**
- * Chequea que la humedad no exeda los rangos minimos y maximos 
- * caso contrario, actua en consecuencia llamando a los actuadores
- * correspondientes.
- */
-void chequear_humedad(){};
+   Chequea que la humedad no exeda los rangos minimos y maximos
+   caso contrario, actua en consecuencia llamando a los actuadores
+   correspondientes.
+*/
+void chequear_humedad() {
+  //Si excede el valor maximo activar buzzer
+  int valorHumedad = digitalRead(humedad01);
+  if (valorHumedad > HUMEDAD_MAX) {
+    //activar_buzzer();
+  }
+};
 
 /**
- * Chequea que la luminosidad no exeda los rangos minimos y maximos
- * caso contrario, actua en consecuencia llamando a los actuadores
- * correspondientes
- */
-void chequear_luminosidad(){};
+   Chequea que la luminosidad no exeda los rangos minimos y maximos
+   caso contrario, actua en consecuencia llamando a los actuadores
+   correspondientes
+*/
+void chequear_luminosidad() {};
 
-void encender_apagar_led(byte led, int veces){
-  unsigned long t_actual; 
-  unsigned long t_previo = 0; 
+void encender_apagar_led(byte led, int veces) {
+  unsigned long t_actual;
+  unsigned long t_previo = 0;
   unsigned long interval = 750;
-  int cumplidas=0;
+  int cumplidas = 0;
   int estado_proximo;
-  while(cumplidas<veces+1){   
+  while (cumplidas < veces + 1) {
     t_actual = millis();
-    if( intervalo_cumplido_p(t_actual, t_previo, interval) ){
+    if ( intervalo_particular_cumplido(t_actual, t_previo, interval) ) {
       Serial.print("\n*************** Intervalo cumplido, PRENDER*************");
       estado_proximo = !digitalRead(led);
-      digitalWrite(led, estado_proximo); 
+      digitalWrite(led, estado_proximo);
       t_previo = t_actual;
-      if(estado_proximo==encendido){
+      if (estado_proximo == encendido) {
         cumplidas++;
-      } 
-    }  
+      }
+    }
   }
   digitalWrite(led, LOW);
-    
-}
 
-int ledApagado(byte led){
-  return digitalRead(led)==0;   
-}
-
-
-void actuar_switch01(){
-  byte switchPulsado = digitalRead(switch01);
-  if(switchPulsado && ledApagado(LED_Blue01)){
-    encender_apagar_led(LED_Blue01, 1); 
-  }
-}
-
-
-void actuar_switch02(){
-  byte switchPulsado = digitalRead(switch02);
-  if(switchPulsado && ledApagado(LED_Blue01)){
-    encender_apagar_led(LED_Blue01, 2); 
-  }
 }
 
 /**
-Este metodo se usa para identificar que alguno de los switch
-han sido desactivados, es decir, se tomo la caja de medicamentos
-de uno de los slots
+   Funcion que enciende el buzzer para que emita sonido por una determinada cantidad
+   de tiempo
+   Parametros:
+   - tiempo: es la cantidad de tiempo en milisegundos que debe permanecer encendido el buzzer
 */
-void chequear_extraccion(){
-  actuar_switch01();
-  actuar_switch02();
+void encender_buzzer(int tiempo) {
+  unsigned long t_actual;
+  unsigned long t_previo = 0;
+  unsigned long interval = 350;
+  int cumplidas = 0;
+  int estado_proximo;
+  
+  t_actual = millis();
+  digitalWrite(buzzer01, HIGH);
+  if ( intervalo_particular_cumplido(t_actual, t_previo, interval) ) {
+    digitalWrite(buzzer01, LOW);  
+  }  
+}
+
+
+
+int ledApagado(byte led) {
+  return digitalRead(led) == 0;
+}
+
+
+void actuar_switch01() {
+  byte switchPulsado = digitalRead(switch01);
+  if (switchPulsado && ledApagado(LED_Blue01)) {
+    encender_apagar_led(LED_Blue01, 1);
+  }
+}
+
+
+void actuar_switch02() {
+  byte switchPulsado = digitalRead(switch02);
+  if (switchPulsado && ledApagado(LED_Blue01)) {
+    encender_apagar_led(LED_Blue01, 2);
+  }
 }
 
 /**
- * Realiza la apertura de una puerta, es decir llama a actuar al servomotor
- * para que mueva un pestillo
- */
-void chequear_apertura(){
-  if(abrir_pestillo == 1){
-    digitalWrite(servo01, HIGH); 
-    //abrir_pestillo = 0;   
-  }else{
+  Este metodo se usa para identificar que alguno de los switch
+  han sido desactivados, es decir, se tomo la caja de medicamentos
+  de uno de los slots
+*/
+void chequear_extraccion() {
+  //actuar_switch01();
+  //actuar_switch02();
+}
+
+/**
+   Realiza la apertura de una puerta, es decir llama a actuar al servomotor
+   para que mueva un pestillo
+*/
+void chequear_apertura() {
+  if (abrir_pestillo == 1) {
+    digitalWrite(servo01, HIGH);
+    //abrir_pestillo = 0;
+  } else {
     digitalWrite(LED_Blue01, LOW);
   }
 }
 
 
 
-void chequear_pulsador(){
+void chequear_pulsador() {
   //int v_ldr = digitalRead(pulsador01);
   delay(50);
-  int v_pulsador = digitalRead(pulsador01); 
-  if( v_pulsador == HIGH and digitalRead(servo01)==LOW ){
-    //abrir_pestillo = 1;  
-    digitalWrite(servo01, HIGH); 
+  int v_pulsador = digitalRead(pulsador01);
+  if ( v_pulsador == HIGH and digitalRead(servo01) == LOW ) {
+    //abrir_pestillo = 1;
+    digitalWrite(servo01, HIGH);
 
-  }else{
-    //abrir_pestillo = 0;   
-    digitalWrite(servo01, LOW); 
+  } else {
+    //abrir_pestillo = 0;
+    digitalWrite(servo01, LOW);
   }
-  Serial.print("\nvalor pulsador: "); 
-  Serial.print(v_pulsador); 
-  
+  Serial.print("\nvalor pulsador: ");
+  Serial.print(v_pulsador);
+
 }
 
 
 void loop() {
-  // put your main code here, to run repeatedly: 
+  // put your main code here, to run repeatedly:
   tiempo = millis();
-  if( intervalo_cumplido() ){
-    //chequear_humedad(); 
+  if ( intervalo_cumplido() ) {
+    //chequear_humedad();
     //chequear_luminosidad();
-    chequear_extraccion(); 
-    //chequear_pulsador(); 
+    //chequear_extraccion();
+    //chequear_pulsador();
     //chequear_apertura();
-    tiempo_anterior = tiempo; 
+    chequear_encender_buzzer();
+    tiempo_anterior = tiempo;
+  }
+}
+
+void chequear_encender_buzzer(){
+  byte switchPulsado = digitalRead(switch01);
+  if (switchPulsado) {
+    tone(buzzer01, 550, 2000); 
   }
 }
